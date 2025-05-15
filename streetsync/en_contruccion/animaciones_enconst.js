7db0f6412285e43d0a107aef5c5c515621365d72
Thomys <124540978+thomys57364@users.noreply.github.com>
2025-05-14 19:58:51 -0500

    Delete streetsync directory

--- streetsync/en_contruccion/animaciones_enconst.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load', () => {
-    const elementosSlide = document.querySelectorAll('.slide-in-left');
-    elementosSlide.forEach((el) => el.classList.add('visible-slide'));
-  });
-  
