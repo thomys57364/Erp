002a5e1fd48b5170ed5747b9db31219315d89045
Thomys <tc57364@gmail.com>
2025-05-15 12:13:08 -0500

    hermosa tabla

--- streetsync/modulos/clientes/cliente.php
+++ streetsync/modulos/clientes/cliente.php
@@ -21,7 +21,7 @@ if (isset($_POST['buscar'])) {
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Modulo Clientes/Proveedores</title>
-  <link rel="stylesheet" href="estilos_cliente.css?v=1" />
+  <link rel="stylesheet" href="estilos_cliente.css?v=2" />
   <link rel="icon" href="../../../imagenes/logo-transparent.png" type="image/png" />
 </head>
 <body>
